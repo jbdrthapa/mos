a0c1d552b910c889f772ddfce56e02c96239a0c6
Jamir Bahadur Thapa <soundhermit@gmail.com>
2026-08-16 12:07:41 -0700

    Adding device description for the audio speakers and microphones

--- widget/modules-right/AudioControlsWidget.tsx
+++ widget/modules-right/AudioControlsWidget.tsx
@@ -10,22 +10,26 @@ export function AudioControlsWidget() {
 
     // simple component for a slider with an icon, used for both speaker and microphone
     function SliderSet({ endpoint }: { endpoint: AstalWp.Endpoint }) {
+        const device = createBinding(endpoint, "device").as((value) => value?.description || "");
         const volumeIcon = createBinding(endpoint, "volumeIcon");
         const volume = createBinding(endpoint, "volume");
         const volumeText = volume.as((value) => String(Math.trunc(value * 100)));
 
         return (
-            <box orientation={Gtk.Orientation.HORIZONTAL}  heightRequest={80}>
+            <box orientation={Gtk.Orientation.HORIZONTAL} heightRequest={80}>
                 <image
                     pixelSize={28}
                     iconName={volumeIcon} cssName={"audio-icon"} />
                 <label valign={Gtk.Align.CENTER} label={volumeText} cssName={"audio-percent"} />
-                <slider
-                    cssClasses={["slider-control"]}
-                    tooltipText={volumeText}
-                    widthRequest={320}
-                    onChangeValue={({ value }) => endpoint.set_volume(value)}
-                    value={createBinding(endpoint, "volume")} />
+                <box orientation={Gtk.Orientation.VERTICAL}>
+                    <label xalign={0} label={device} tooltipText={device} cssName="slider-device-name" />
+                    <slider
+                        cssClasses={["slider-control"]}
+                        tooltipText={volumeText}
+                        widthRequest={320}
+                        onChangeValue={({ value }) => endpoint.set_volume(value)}
+                        value={createBinding(endpoint, "volume")} />
+                </box>
             </box>
         );
     }
